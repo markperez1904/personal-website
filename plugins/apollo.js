7a2d5426decac6777043a8fd0f9ec24e40071919
Mark Perez <markperez1904@gmail.com>
2019-08-28 17:22:40 -0700

    updated nuxt module

--- plugins/apollo.js
+++ plugins/apollo.js
@@ -3,10 +3,10 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import ApolloClient from 'apollo-client'
 
 const client = new ApolloClient({
-	link: PrismicLink({
-		uri: 'https://marks-personal-website.prismic.io/graphql'
-	}),
-	cache: new InMemoryCache()
+  link: PrismicLink({
+    uri: 'https://marks-personal-website.prismic.io/graphql'
+  }),
+  cache: new InMemoryCache()
 })
 
 export default () => client
