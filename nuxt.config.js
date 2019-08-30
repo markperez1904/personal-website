c8f68f0e2408438d14e0e08674a8d4d18af7a459
Mark Perez <markperez1904@gmail.com>
2019-08-30 14:52:55 -0700

    enabled zoom, but disabled double-tap to zoom

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
       {
         name: 'viewport',
         content:
-          'width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0'
+          'width=device-width, initial-scale=1, maximum-scale=1.0'
       },
       {
         hid: 'description',
