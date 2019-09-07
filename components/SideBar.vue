c54f3e4cf59d2c839a18d4db6a7d4cc100f0baa4
Mark Perez <markperez1904@gmail.com>
2019-09-07 05:00:49 -0700

    rearranged sidebar

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -3,6 +3,21 @@
     <!-- Email Lead Generator -->
     <app-email></app-email>
 
+    <!-- PayPal Link -->
+    <aside class="column is-narrow is-12-widescreen is-12-desktop">
+      <a
+        href="https://www.paypal.com/paypalme2/markperez1904"
+        target="_blank"
+        aria-label="paypal"
+        rel="noopener"
+      >
+        <img
+          src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png"
+          alt="PayPal"
+        />
+      </a>
+    </aside>
+
     <!-- YouTube Subscription Button -->
     <script src="https://apis.google.com/js/platform.js"></script>
     <section class="columns is-multiline is-vcentered is-centered">
@@ -13,21 +28,6 @@
           data-layout="full"
           data-count="default"
         ></div>
-      </aside>
-
-      <!-- PayPal Link -->
-      <aside class="column is-narrow is-12-widescreen is-12-desktop">
-        <a
-          href="https://www.paypal.com/paypalme2/markperez1904"
-          target="_blank"
-          aria-label="paypal"
-          rel="noopener"
-        >
-          <img
-            src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png"
-            alt="PayPal"
-          />
-        </a>
       </aside>
 
       <!-- AdSense component -->
