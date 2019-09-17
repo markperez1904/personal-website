a18a98c3ac3c2c98ceb454f5ea0887785dc8488f
Mark Perez <markperez1904@gmail.com>
2019-09-16 19:28:49 -0700

    added logitech affiliate banner

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -32,7 +32,7 @@
 
       <!-- AdSense component -->
       <aside class="column is-narrow is-12-widescreen is-12-desktop">
-        <adsbygoogle :ad-slot="2763616750" />
+        <adsbygoogle ad-slot="2763616750" />
       </aside>
     </section>
   </div>
--- pages/blog/_slug/index.vue
+++ pages/blog/_slug/index.vue
@@ -4,6 +4,22 @@
     <section class="columns is-centered is-multiline">
       <!-- Blog Post -->
       <article class="column is-7">
+        <!-- CJ Affiliate Banner -->
+        <figure class="banner">
+          <a
+            href="http://www.kqzyfj.com/click-9174765-10658620"
+            target="_blank"
+            rel="noopener"
+            aria-label="logitech banner"
+          >
+            <img
+              class="box banner-image"
+              src="http://www.awltovhc.com/image-9174765-10658620"
+              alt="Shop for Keyboards, Mice, and more at Logitech"
+            />
+          </a>
+        </figure>
+
         <!-- Title, Date, and Description -->
         <h1>{{ blog_posts.title[0].text }}</h1>
         <p class="date">{{ blog_posts.date | moment("MMM DD, YYYY") }}</p>
@@ -119,6 +135,20 @@ export default {
   margin: 1rem 0;
   border-radius: 6px;
   box-shadow: 0px 2px 18px 0px rgba(0, 0, 0, 0.3);
+}
+
+.banner {
+  text-align: center;
+  margin: 1rem 0 0 0;
+}
+
+.banner .box {
+  padding: unset;
+  display: inline-block;
+}
+
+.banner-image {
+  border-radius: 6px;
 }
 
 h1 {
