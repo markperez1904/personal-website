a25130fbff8a6f99fceefd2326ac0c5f35749009
Mark Perez <markperez1904@gmail.com>
2020-10-22 02:34:30 -0700

    new target for nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,9 +5,9 @@ import gql from 'graphql-tag'
 
 const client = new ApolloClient({
   link: PrismicLink({
-    uri: 'https://marks-personal-website.prismic.io/graphql',
+    uri: 'https://marks-personal-website.prismic.io/graphql'
   }),
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache()
 })
 
 const linkages = () => {
@@ -25,17 +25,18 @@ const linkages = () => {
             }
           }
         }
-      `,
+      `
     })
-    .then((res) => {
-      return res.data.allBlog_postss.edges.map((posts) => {
+    .then(res => {
+      return res.data.allBlog_postss.edges.map(posts => {
         return '/blog/' + posts.node._meta.uid
       })
     })
 }
 
 export default {
-  mode: 'universal',
+  // full static mode (as of v2.14.0): https://nuxtjs.org/blog/going-full-static/
+  target: 'static',
 
   // Headers of the page
   head: {
@@ -44,17 +45,17 @@ export default {
       { charset: 'utf-8' },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1, shrink-to-fit=no',
+        content: 'width=device-width, initial-scale=1, shrink-to-fit=no'
       },
       {
         name: 'google-site-verification',
-        content: process.env.GOOGLE_VERIFICATION,
+        content: process.env.GOOGLE_VERIFICATION
       },
       {
         hid: 'og:site_name',
         property: 'og:site_name',
-        content: 'Mark Perez',
-      },
+        content: 'Mark Perez'
+      }
     ],
     // Link to Google Fonts and Website Image
     link: [
@@ -65,8 +66,8 @@ export default {
       {
         rel: 'stylesheet',
         href:
-          'https://fonts.googleapis.com/css?family=Oxygen|Oxygen+Mono&display=swap',
-      },
+          'https://fonts.googleapis.com/css?family=Oxygen|Oxygen+Mono&display=swap'
+      }
     ],
 
     script: [
@@ -83,12 +84,12 @@ export default {
           email: 'info@markperez.dev',
           sameAs: [
             'https://www.youtube.com/channel/UCCZ1Yo4NOdWW03jamsNFA8w',
-            'https://github.com/markperez1904',
-          ],
-        }),
-      },
+            'https://github.com/markperez1904'
+          ]
+        })
+      }
     ],
-    __dangerouslyDisableSanitizers: ['script'],
+    __dangerouslyDisableSanitizers: ['script']
   },
 
   // Customize the progress-bar color on mobile devices
@@ -102,7 +103,7 @@ export default {
     '~/plugins/disqus.js',
     '~/plugins/moment.js',
     '~/plugins/typer.js',
-    '~/plugins/spinner.js',
+    '~/plugins/spinner.js'
   ],
 
   // Nuxt.js modules
@@ -115,37 +116,37 @@ export default {
     '@nuxtjs/google-adsense',
     '@nuxtjs/sitemap',
     '@nuxtjs/robots',
-    'nuxt-purgecss',
+    'nuxt-purgecss'
   ],
 
   // Keep Tracking ID private
   googleAnalytics: {
-    id: process.env.GOOGLE_ANALYTICS,
+    id: process.env.GOOGLE_ANALYTICS
   },
 
   // Keep Google AdSense code private
   'google-adsense': {
     id: process.env.GOOGLE_ADSENSE_PUBLISHER,
-    analyticsUacct: process.env.GOOGLE_ANALYTICS,
+    analyticsUacct: process.env.GOOGLE_ANALYTICS
   },
 
   // Using apollo to query blog posts
   apollo: {
     clientConfigs: {
-      default: '~/plugins/apollo.js',
-    },
+      default: '~/plugins/apollo.js'
+    }
   },
 
   // Generate index.html files for each blog post
   generate: {
-    routes: linkages,
+    routes: linkages
   },
 
   // generate sitemap.xml for Search Console
   sitemap: {
     hostname: 'https://markperez.dev',
     routes: linkages,
-    trailingSlash: true,
+    trailingSlash: true
   },
 
   // tell web crawlers where to crawl
@@ -154,13 +155,13 @@ export default {
 
     Disallow: () => ['/thanks/', '/200.html', '/README.md'],
 
-    Sitemap: 'https://markperez.dev/sitemap.xml',
+    Sitemap: 'https://markperez.dev/sitemap.xml'
   },
 
   // purge unnecessary CSS
   purgeCSS: {
     mode: 'postcss',
     paths: ['static/css/main.css'], // whitelist the main.css file
-    whitelistPatterns: [/-leave/, /-enter/], // whitelist the page transition CSS classes
-  },
+    whitelistPatterns: [/-leave/, /-enter/] // whitelist the page transition CSS classes
+  }
 }
